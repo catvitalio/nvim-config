87811f1887c363a7a54f4d4b66e32c61b7dd5deb
Vitaly Kotov <catvitalio@gmail.com>
2024-02-28 06:51:39 +0700

    Telescope conf

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -2,7 +2,6 @@ vim.g.mapleader = ' '
 
 -- NeoTree
 vim.keymap.set('n', '<leader>e', ':Neotree float reveal<CR>')
-vim.keymap.set('n', '<leader>o', ':Neotree float git_status<CR>')
 
 -- Navigation
 vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
@@ -32,10 +31,8 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fw', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>gb', builtin.git_branches, {})
 vim.keymap.set('n', '<leader>gc', builtin.git_commits, {})
 vim.keymap.set('n', '<leader>gs', builtin.git_status, {})
-vim.keymap.set('n', '<leader>ls', builtin.lsp_document_symbols, {})
 vim.keymap.set('n', 'gr', builtin.lsp_references, { noremap = true, silent = true })
 vim.keymap.set('n', 'gd', builtin.lsp_definitions, { noremap = true, silent = true })
