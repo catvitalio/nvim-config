ac11d9a2968690db00f66d0850e09b876b0190ae
Vitaly Kotov <catvitalio@gmail.com>
2024-08-19 22:37:30 +0700

    Fix neotree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -11,6 +11,13 @@ return {
       require('neo-tree').setup({
         close_if_last_window = true,
         popup_border_style = 'rounded',
+        filesystem = {
+          hijack_netrw_behavior = 'open_default',
+          group_empty_dirs = true,
+        },
+        window = {
+          position = 'float',
+        },
       })
     end,
   },
