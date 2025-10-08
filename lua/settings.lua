5e89e5f1a0ca85112c0c6ccded9508f5ad274757
Vitaly Kotov <catvitalio@gmail.com>
2025-10-08 12:05:02 +0700

    change cmd height

--- lua/settings.lua
+++ lua/settings.lua
@@ -10,6 +10,9 @@ vim.o.mousemoveevent = true
 vim.opt.fillchars = { eob = ' ' }
 vim.opt.termsync = false
 
+-- Hide cmd line
+vim.opt.cmdheight = 0
+
 -- Fix color around borders
 vim.api.nvim_set_hl(0, 'NormalFloat', { link = 'Normal' })
 vim.api.nvim_set_hl(0, 'FloatBorder', { link = 'Normal' })
