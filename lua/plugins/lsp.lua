8407a6d5209067d4e07aa7ad722d7933df51ee0e
Vitaly Kotov <catvitalio@gmail.com>
2025-11-18 08:45:54 +0700

    remove zuban

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,23 +1,15 @@
 local function config()
   -- python
-  vim.lsp.config('zuban', {
-    on_attach = function(client, bufnr)
-      client.server_capabilities.documentFormattingProvider = false
-      client.server_capabilities.documentRangeFormattingProvider = false
-      client.server_capabilities.renameProvider = false
-      client.server_capabilities.codeActionProvider = false
-      client.server_capabilities.completionProvider = nil
-      client.server_capabilities.hoverProvider = false
-      client.server_capabilities.signatureHelpProvider = nil
-      client.server_capabilities.definitionProvider = false
-      client.server_capabilities.declarationProvider = false
-      client.server_capabilities.implementationProvider = false
-      client.server_capabilities.referencesProvider = false
-      client.server_capabilities.documentSymbolProvider = false
-      client.server_capabilities.workspaceSymbolProvider = false
-    end,
+  vim.lsp.config('pyrefly', {
+    settings = {
+      python = {
+        pyrefly = {
+          displayTypeErrors = 'force-on',
+          logLevel = 'warning',
+        },
+      },
+    },
   })
-  vim.lsp.enable('zuban')
   vim.lsp.enable('pyrefly')
   -- lua
   vim.lsp.enable('lua_ls')
