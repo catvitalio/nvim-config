dbc72bb7bcc83a7f35eec5e053c4bc3744460661
Vitaly Kotov <catvitalio@gmail.com>
2026-02-14 04:43:36 +0700

    remove cursor theme

--- lua/themes/cursor.lua
+++
@@ -1,7 +0,0 @@
-return function()
-  local utils = require('themes.utils')
-  local opts = { style = 'dark', dashboard = false }
-  local colors = {}
-
-  return utils.setup_theme('ydkulks/cursor-dark.nvim', opts, colors)
-end
