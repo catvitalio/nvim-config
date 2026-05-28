fb6ec1de8e3c82823bb399f893be78fdd13e8b85
Vitaly Kotov <catvitalio@gmail.com>
2026-05-29 00:24:26 +0700

    gitsigns delay

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -7,7 +7,7 @@ return {
       current_line_blame_opts = {
         virt_text = true,
         virt_text_pos = 'eol',
-        delay = 100,
+        delay = 1,
         ignore_whitespace = false,
         virt_text_priority = 100,
       },
